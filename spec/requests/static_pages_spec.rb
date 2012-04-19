2cc9eecf59f173fd9298dc08ce1e537c02cabb1d
perfectionist <loeffler@mac.com>
2012-04-19 08:58:25 -0500

    Completed layout sections of chapter 5 - fixed full_title arguments.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -17,20 +17,20 @@ describe "StaticPages" do
     before { visit help_path }
 
     it { should have_selector('h1', text: 'Help') }
-    it { should have_selector('title', text: full_title('')) }
+    it { should have_selector('title', text: full_title('Help')) }
   end
 
   describe "About page" do
     before { visit about_path }
 
     it { should have_selector('h1', text: 'About Us') }
-    it { should have_selector('title', text: full_title('')) }
+    it { should have_selector('title', text: full_title('About Us')) }
   end
 
   describe "Contact page" do
     before { visit contact_path }
 
     it { should have_selector('h1', text: 'Contact') }
-    it { should have_selector('title', text: full_title('')) }
+    it { should have_selector('title', text: full_title('Contact')) }
   end
 end
