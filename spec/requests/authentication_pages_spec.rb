887880ac5b1fba5047d19af5b3dec80d5f1f045a
perfectionist <loeffler@mac.com>
2012-05-16 08:09:24 -0500

    Fix bug in authentication test.

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -13,10 +13,10 @@ describe "Authentication" do
 
   describe "signin" do
     before { visit signin_path }
-    
+
     describe "with invalid information" do
       before { click_button 'Sign in' }
-      
+
       it { should have_selector('title', text: 'Sign in') }
       it { should have_selector('div.alert.alert-error', text: 'Invalid') }
 
@@ -29,18 +29,24 @@ describe "Authentication" do
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
       before do
-        fill_in "Email",    with: user.email
+        fill_in "Email", with: user.email
         fill_in "Password", with: user.password
         click_button 'Sign in'
       end
 
       it { should have_selector('title', text: user.name) }
       it { should have_link('Profile', href: user_path(user)) }
+      it { should have_link('Settings', href: edit_user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
-    end
-  end
 
+      describe "followed by signout" do
+        before { click_link 'Sign out' }
+        it { should have_link('Sign in') }
+      end
+    end
+
+  end
 
 
   #describe "GET /authentication_pages" do
